2a05389a53576f584c5168a08391d8b7af3144d9
satish-313 <pradhansatish53@gmail.com>
2022-09-25 22:54:01 +0530

    post page 2 hour

--- web/pages/post/[slug].tsx
+++ web/pages/post/[slug].tsx
@@ -1,9 +1,114 @@
-import React from 'react'
+import { GetStaticProps } from "next";
+import Header from "../../components/Header";
+import { sanityClient, urlFor } from "../../sanity";
+import { post } from "../../typings";
+import PortableText from "react-portable-text";
 
-function Post() {
-  return (
-    <div>Post</div>
-  )
+interface Props {
+  post: post;
 }
 
-export default Post
+function Post({ post }: Props) {
+  return (
+    <main>
+      <Header />
+      <img
+        className="w-full h-40 object-cover"
+        src={urlFor(post.mainImage).url()!}
+        alt=""
+      />
+      <article className="max-w-3xl p-5 mx-auto">
+        <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
+        <h2 className="text-xl font-light text-gray-600">{post.description}</h2>
+        <div className="flex items-center space-x-2">
+          <img
+            className="h-10 w-10 rounded-full"
+            src={urlFor(post.author.image).url()!}
+            alt=""
+          />
+          <p className="font-extralight text-sm">
+            written by{" "}
+            <span className="text-green-600">{post.author.name}</span>-
+            Published at {new Date(post._createdAt).toLocaleDateString()}
+          </p>
+        </div>
+        <div className="mt-10">
+          <PortableText
+            className=""
+            dataset={process.env.NEXT_PUBLIC_SANTITY_DATASET!}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+            content={post.body}
+            serializers={{
+              h1: (props: any) => (
+                <h1 className="text-2xl font-bold my-5" {...props} />
+              ),
+              h2: (props: any) => (
+                <h2 className="text-xl font-bold my-5" {...props} />
+              ),
+              li: (children: any) => (
+                <li className="ml-4 list-disc">{children}</li>
+              ),
+              link: ({ href, children }: any) => (
+                <a href={href} className="text-blue-500 hover:underline">
+                  {children}
+                </a>
+              ),
+            }}
+          />
+        </div>
+      </article>
+    </main>
+  );
+}
+
+export default Post;
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "post"]{
+    _id,
+    slug {
+    current
+    }
+  }`;
+
+  const posts: post[] = await sanityClient.fetch(query);
+  const paths = posts.map((p) => ({
+    params: {
+      slug: p.slug.current,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const query = `*[_type=="post" && slug.current == $slug][0]{
+    _id,
+    _createdAt,
+    title,
+    author-> {
+    name,
+    image
+  },
+    description,
+    mainImage,
+    slug,
+    body
+  }`;
+
+  const post = await sanityClient.fetch(query, { slug: params?.slug });
+
+  if (!post) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { post },
+    revalidate: 60 * 60 * 24, // 24 hour or 1day
+  };
+};
